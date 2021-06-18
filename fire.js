3b1765b8954c61bec3054111f882f4a7e3492186
kridtapote33 <86095246+kridtapote33@users.noreply.github.com>
2021-06-18 22:48:29 +0700

    Update fire.js

--- fire.js
+++ fire.js
@@ -1,12 +1,12 @@
 var firebase = require('firebase')
 // change lines below with your own Firebase snippets!
 var config = {
-  apiKey: "AIzaSyDSLRypcYx2yHSqpVXJMBrzYw1xAHZtgYI",
-  authDomain: "test-iot-esp.firebaseapp.com",
-  databaseURL: "https://test-iot-esp.firebaseio.com",
-  projectId: "test-iot-esp",
-  storageBucket: "test-iot-esp.appspot.com",
-  messagingSenderId: "467772798973"
+  apiKey: "AIzaSyDKj2IQVBr85A2eWPUUbKxUZOiX9NHiCM8",
+  authDomain: "hydro-3b018.firebaseapp.com",
+  databaseURL: "https://hydro-3b018-default-rtdb.firebaseio.com",
+  projectId: "hydro-3b018",
+  storageBucket: "hydro-3b018.appspot.com",
+  messagingSenderId: "1005033448393"
 };
 const fire = firebase.initializeApp(config);
 module.exports = fire;
